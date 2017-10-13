b6343561d86b97e31a516bd35822ebc490e732be
sundayj <justin.sunday@live.com>
2017-10-13 14:10:22 -0400

    Switched from iframe to jQuery include
    Since the iframe wasn't wanting to work on OSX, I switched to using a div (which is easier to fit in with sizing than the iframe anyway) and using jQuery to grab the individual html pages and setting the html into the div I created. I had to change some file paths, but it actually made the modularity easier and more closely resembles an application.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,33 @@
-$(document).foundation()
+$(document).foundation();
+
+$(document).ready(function () {
+
+    $.get("student-views/instruments-students.html", function (data) {
+        $("#studentview").html(data);
+    });
+
+    $("#testbtn").click(function () {
+        $.get("student-views/tests-student.html", function (data) {
+            $("#studentview").html(data);
+        });
+    });
+
+    $("#notebtn").click(function () {
+        $.get("student-views/notes-student.html", function (data) {
+            $("#studentview").html(data);
+        });
+    });
+
+    $("#eventbtn").click(function () {
+        $.get("student-views/events-student.html", function (data) {
+            $("#studentview").html(data);
+        });
+    });
+
+    //    $("#notificationbtn").click(function(){
+    //        
+    //    });
+
+
+
+});
